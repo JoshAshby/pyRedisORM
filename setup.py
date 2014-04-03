d8017cb3bb2576161b879b18e232159df8cf8549
JoshAshby <joshuaashby@joshashby.com>
2014-04-02 21:52:34 -0600

    setup stuff stolen from webob
    add test file that does nothing for right now

--- setup.py
+++ setup.py
@@ -1,17 +1,40 @@
-from distutils.core import setup
-import redisORM
+from setuptools import setup
+
+version = '0.1.0'
+
+testing_extras = ['nose', 'coverage']
+
+docs_extras = ['Sphinx']
 
 setup(
     name='RedisORM',
-    version=redisORM.__version__,
+    version=version,
+    description="Simple little ORM thing for Redis",
+    long_description="""\
+RedisORM provides a simple ORM, or what I like to call a: Key-Value to Grouped Object Mapper.
+Currently it just supports strings and lists but I plan on supporting more later on.
+
+* `Docs <https://pyredisorm.readthedocs.org/en/latest/>`_
+* `Bug tracker <https://github.com/JoshAshby/pyRedisORM/issues>`_
+* `Browse source code <https://github.com/JoshAshby/pyRedisORM/>`_
+""",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+    ],
+    keywords='redis orm database',
     author='Joshua P Ashby',
     author_email='joshuaashby@joshashby.com',
-    packages=['redisORM', 'redisORM.tests'],
-    url='https://github.com/JoshAshby/pyRedisORM',
-    license='MIT (See LICENSE.txt for more info)',
-    description='Simple little ORM for working with Redis',
-    long_description=open('README.rst').read(),
-    install_requires=[
-        "nose >= 1.3.0"
-    ],
+    maintainer='Joshua P Ashby',
+    url='https://github.com/JoshAshby/pyRedisORM/',
+    license='MIT',
+    packages=['redisORM'],
+    zip_safe=True,
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    extras_require = {
+        'testing':testing_extras,
+        'docs':docs_extras,
+        },
 )
---
+++ tests/test_model.py
@@ -0,0 +1,3 @@
+import redis
+from nose.tools import eq_, assert_raises
+from redisORM import redis_model
